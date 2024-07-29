3cf88100424051bce431a370761e9570d0349aff
Maleesha Herath <91375598+MaleeshaHe@users.noreply.github.com>
2024-07-29 12:54:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
-const app = express();
 const port = process.env.PORT || 3001;
+const app = express();
 
+app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/",(req,res) => {
-    res.send("server is ready!");
+app.get("/", (req, res) => {
+  res.send("server is ready!");
 });
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
