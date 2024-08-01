cd36703153d7930e05b0d551af928c5d11504eff
Maleesha Herath <91375598+MaleeshaHe@users.noreply.github.com>
2024-08-01 16:02:11 +0530

    create admin login function

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,8 +1,15 @@
-var bcrypt = require("bcrypt");
-var validator = require("fastest-validator");
-var { User, Role, Permission, UserPermissions } = require("../models");
-const { where } = require("sequelize");
-const sendEmail = require('../utils/email');
+const bcrypt = require("bcrypt");
+const validator = require("fastest-validator");
+const jwt = require("jsonwebtoken");
+const {
+  User,
+  Role,
+  Permission,
+  UserPermissions,
+  UserRoles,
+} = require("../models");
+
+const sendEmail = require("../utils/email");
 
 const v = new validator();
 
@@ -11,17 +18,17 @@ function addAdmin(req, res) {
 
   // Validate request
   const schema = {
-    name: { type: 'string', min: 3, max: 255 },
-    email: { type: 'email', max: 255 },
-    password: { type: 'string', min: 6, max: 255 },
-    roles: { type: 'array', items: 'string', min: 1 },
+    name: { type: "string", min: 3, max: 255 },
+    email: { type: "email", max: 255 },
+    password: { type: "string", min: 6, max: 255 },
+    roles: { type: "array", items: "string", min: 1 },
   };
 
   const check = v.validate(req.body, schema);
 
   if (check !== true) {
     return res.status(400).send({
-      message: 'Validation failed',
+      message: "Validation failed",
       errors: check,
     });
   }
@@ -32,9 +39,9 @@ function addAdmin(req, res) {
     .then(function (user) {
       if (user.length > 0) {
         return res.status(400).send({
-          message: 'User already exists',
+          message: "User already exists",
           errors: [
-            { field: email, message: 'User with this email already exists' },
+            { field: email, message: "User with this email already exists" },
           ],
         });
       }
@@ -55,55 +62,67 @@ function addAdmin(req, res) {
           Role.findAll({
             where: { name: roles },
             include: [Permission], // Include permissions related to these roles
-          }).then(async function (roleInstances) {
-            // Add roles to the user
-            await user.addRoles(roleInstances);
+          })
+            .then(async function (roleInstances) {
+              // Add roles to the user
+              await user.addRoles(roleInstances);
 
-            // Extract permissions from roles
-            var permissionsToAdd = [];
-            var permissionNames = [];
-            roleInstances.forEach((role) => {
-              role.Permissions.forEach((permission) => {
-                permissionsToAdd.push({
-                  UserId: user.id,
-                  PermissionId: permission.id,
+              // Extract permissions from roles
+              var permissionsToAdd = [];
+              var permissionNames = [];
+              roleInstances.forEach((role) => {
+                role.Permissions.forEach((permission) => {
+                  permissionsToAdd.push({
+                    UserId: user.id,
+                    PermissionId: permission.id,
+                  });
+                  permissionNames.push(permission.name);
                 });
-                permissionNames.push(permission.name);
+              });
+
+              // Bulk create user permissions
+              await UserPermissions.bulkCreate(permissionsToAdd);
+
+              // Extract domain name from email
+              const siteName = process.env.SITE_NAME;
+
+              // Send email notification
+              await sendEmail({
+                to: email,
+                subject: "Admin Account Created",
+                text: `Hello ${name},\n\nYou are now an admin on ${siteName}.\n\nYou have been assigned the following roles: ${roles.join(
+                  ", "
+                )}.\nYour permissions include: ${permissionNames.join(
+                  ", "
+                )}.\n\nBest regards,\nYour Company`,
+                html: `<p>Hello ${name},</p><p>You are now an admin on ${siteName}.</p><p>You have been assigned the following roles: ${roles.join(
+                  ", "
+                )}.</p><p>Your permissions include: ${permissionNames.join(
+                  ", "
+                )}.</p><p>Best regards,<br>Your Company</p>`,
+              });
+
+              res.status(201).send({
+                message: "Admin and permissions created successfully!",
+                user: user,
+              });
+            })
+            .catch(function (error) {
+              res.status(500).send({
+                message: "Error assigning roles to admin",
+                error: error.message,
               });
             });
-
-            // Bulk create user permissions
-            await UserPermissions.bulkCreate(permissionsToAdd);
-
-            // Extract domain name from email
-            const siteName = process.env.SITE_NAME;
-
-            // Send email notification
-            await sendEmail({
-              to: email,
-              subject: 'Admin Account Created',
-              text: `Hello ${name},\n\nYou are now an admin on ${siteName}.\n\nYou have been assigned the following roles: ${roles.join(', ')}.\nYour permissions include: ${permissionNames.join(', ')}.\n\nBest regards,\nYour Company`,
-              html: `<p>Hello ${name},</p><p>You are now an admin on ${siteName}.</p><p>You have been assigned the following roles: ${roles.join(', ')}.</p><p>Your permissions include: ${permissionNames.join(', ')}.</p><p>Best regards,<br>Your Company</p>`,
-            });
-
-            res.status(201).send({
-              message: 'Admin and permissions created successfully!',
-              user: user,
-            });
-          }).catch(function (error) {
-            res.status(500).send({
-              message: 'Error assigning roles to admin',
-              error: error.message,
-            });
-          });
         })
         .catch(function (error) {
-          res.status(500).send({ message: 'Server error', error: error.message });
+          res
+            .status(500)
+            .send({ message: "Server error", error: error.message });
         });
     })
     .catch(function (error) {
       return res.status(500).send({
-        message: 'Server error',
+        message: "Server error",
         error: error.message,
       });
     });
@@ -114,16 +133,16 @@ function changeAdminPassword(req, res) {
 
   // Validate request
   const schema = {
-    email: { type: 'email', max: 255 },
-    oldPassword: { type: 'string', min: 6, max: 255 },
-    newPassword: { type: 'string', min: 6, max: 255 },
+    email: { type: "email", max: 255 },
+    oldPassword: { type: "string", min: 6, max: 255 },
+    newPassword: { type: "string", min: 6, max: 255 },
   };
 
   const check = v.validate(req.body, schema);
 
   if (check !== true) {
     return res.status(400).send({
-      message: 'Validation failed',
+      message: "Validation failed",
       errors: check,
     });
   }
@@ -134,27 +153,27 @@ function changeAdminPassword(req, res) {
     .then(function (user) {
       if (!user) {
         return res.status(404).send({
-          message: 'User not found',
-          errors: [{ field: email, message: 'User not found' }],
+          message: "User not found",
+          errors: [{ field: email, message: "User not found" }],
         });
       }
 
       // Check if old password is correct
       if (!bcrypt.compareSync(oldPassword, user.password)) {
         return res.status(400).send({
-          message: 'Invalid password',
-          errors: [{ field: 'password', message: 'Invalid password' }],
+          message: "Invalid password",
+          errors: [{ field: "password", message: "Invalid password" }],
         });
       }
 
       // Check if new password is the same as old password
       if (bcrypt.compareSync(newPassword, user.password)) {
         return res.status(400).send({
-          message: 'New password cannot be the same as old password',
+          message: "New password cannot be the same as old password",
           errors: [
             {
-              field: 'newPassword',
-              message: 'New password cannot be the same as old password',
+              field: "newPassword",
+              message: "New password cannot be the same as old password",
             },
           ],
         });
@@ -171,25 +190,118 @@ function changeAdminPassword(req, res) {
           // Send email notification
           await sendEmail({
             to: email,
-            subject: 'Password Changed Successfully',
+            subject: "Password Changed Successfully",
             text: `Hello,\n\nYour password has been changed successfully.\n\nIf you did not request this change, please contact support immediately.\n\nBest regards,\nYour Company`,
             html: `<p>Hello,</p><p>Your password has been changed successfully.</p><p>If you did not request this change, please contact support immediately.</p><p>Best regards,<br>Your Company</p>`,
           });
 
           res.status(200).send({
-            message: 'Password changed successfully',
+            message: "Password changed successfully",
           });
         })
         .catch(function (error) {
           res.status(500).send({
-            message: 'Server error',
+            message: "Server error",
             error: error.message,
           });
         });
     })
     .catch(function (error) {
       return res.status(500).send({
-        message: 'Server error',
+        message: "Server error",
+        error: error.message,
+      });
+    });
+}
+
+function adminLogin(req, res) {
+  const { email, password } = req.body;
+
+  // Validate request
+  const schema = {
+    email: { type: "email", max: 255 },
+    password: { type: "string", min: 6, max: 255 },
+  };
+
+  const check = v.validate(req.body, schema);
+
+  if (check !== true) {
+    return res.status(400).send({
+      message: "Validation failed",
+      errors: check,
+    });
+  }
+
+  User.findOne({
+    where: { email: email, is_admin: true },
+    include: [
+      {
+        model: Role,
+        attributes: ["id", "name"], // Fetch role id and name
+        through: { attributes: [] }, // Exclude junction table attributes
+      },
+      {
+        model: Permission,
+        attributes: ["id", "name"], // Fetch permission id and name
+        through: { attributes: [] }, // Exclude junction table attributes
+      },
+    ],
+  })
+    .then(async function (user) {
+      if (!user) {
+        return res.status(404).send({
+          message: "User not found",
+          errors: [{ field: email, message: "User not found" }],
+        });
+      }
+
+      // Check if password is correct
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(400).send({
+          message: "Invalid password",
+          errors: [{ field: "password", message: "Invalid password" }],
+        });
+      }
+
+      // Extract roles and permissions with names
+      const roles = user.Roles.map((role) => ({
+        id: role.id,
+        name: role.name,
+      }));
+
+      const permissions = user.Permissions.map((permission) => ({
+        id: permission.id,
+        name: permission.name,
+      }));
+
+      // Create JWT token
+      const token = jwt.sign(
+        {
+          userId: user.id,
+          email: user.email,
+          roles: roles,
+          permissions: permissions,
+          name: user.name,
+        },
+        process.env.JWT_SECRET_KEY,
+        { expiresIn: process.env.JWT_EXPIRE }
+      );
+
+      // Set the token in an HTTP-only cookie
+      res.cookie("authToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "Strict",
+        maxAge: process.env.COOKIE_EXPIRE,
+      });
+
+      res.status(200).send({
+        message: "Login successful",
+      });
+    })
+    .catch(function (error) {
+      return res.status(500).send({
+        message: "Server error fetching user",
         error: error.message,
       });
     });
@@ -198,4 +310,5 @@ function changeAdminPassword(req, res) {
 module.exports = {
   addAdmin: addAdmin,
   changeAdminPassword: changeAdminPassword,
+  adminLogin: adminLogin,
 };
