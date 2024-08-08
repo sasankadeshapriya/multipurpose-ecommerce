a79cd7d97cd643850fe522b09a7ca50a03ef8acb
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2024-08-09 00:13:20 +0530

    add update brand

--- backend/controllers/brand.controller.js
+++ backend/controllers/brand.controller.js
@@ -1,4 +1,6 @@
 const { Brand } = require("../models");
+const { Op } = require("sequelize"); 
+
 
 function generateSlug(name) {
     return name.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
@@ -46,6 +48,63 @@ function addBrand(req, res) {
     });
 }
 
+async function updateBrand(req, res) {
+    const id = req.params.id;
+    const { brand_name } = req.body;
+
+    if (!id) {
+        return res.status(400).send({ message: "Brand ID is required" });
+    }
+
+    try {
+        const brand = await Brand.findByPk(id);
+        if (!brand) {
+            return res.status(404).send({ message: "Brand not found" });
+        }
+
+        let updateFields = {};
+        let changesMade = false;
+
+        if (brand_name) {
+            if (brand_name.length < 2) {
+                return res.status(400).send({ message: "Brand name must be at least 2 characters long" });
+            }
+            if (brand_name !== brand.brand_name) {
+                const nameExists = await Brand.findOne({
+                    where: { brand_name, id: { [Op.ne]: id } }
+                });
+                if (nameExists) {
+                    return res.status(409).send({ message: "Brand name already exists" });
+                }
+
+                updateFields.brand_name = brand_name;
+                updateFields.brand_slug = generateSlug(brand_name);
+                changesMade = true;
+            }
+        }
+
+        if (req.file) {
+            updateFields.brand_image = req.file.filename;
+            changesMade = true;
+        }
+
+        if (!changesMade) {
+            return res.status(400).send({ message: "No updates provided or no changes detected" });
+        }
+
+        await brand.update(updateFields);
+        res.status(200).send({
+            message: "Brand updated successfully",
+            brand: updateFields
+        });
+        
+    } catch (error) {
+        console.error('Failed to update brand:', error);
+        res.status(500).send({ message: "Server error", error: error.message });
+    }
+}
+
 module.exports = {
-    addBrand: addBrand
+    addBrand: addBrand,
+    updateBrand:updateBrand
 };
