8db7f56ca065fe627e591a6d16a0257e6e9dcec1
MaleeshaHe <malishaherath77@gmail.com>
2024-12-31 17:43:14 +0530

    add digital product route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { physicalProductAdd } = require('../controllers/product.controller');
-const { getUploader } = require('../utils/image-uploader');
+const {
+  physicalProductAdd,
+  digitalProductAdd,
+} = require("../controllers/product.controller");
+const { getUploader } = require("../utils/image-uploader");
 
 // Middleware for image upload specific to product images
 const uploadProductImage = getUploader("products").single("image");
+const uploadProductImages = getUploader("products").array("images", 5);
 
 // Route to add a new physical product
-router.post('/add-physical-product', uploadProductImage, physicalProductAdd);
-
+router.post("/add-physical-product", uploadProductImage, physicalProductAdd);
+router.post("/add-digital-product", uploadProductImages, digitalProductAdd);
 
 module.exports = router;
