30e8051dfdafa6d69c1bac466e8747684b0f7606
MaleeshaHe <malishaherath77@gmail.com>
2025-01-01 17:57:01 +0530

    Create digital product add function

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
 
-router.post('/add-physical-product', productController.insertPhysicalProduct);
+router.post("/add-physical-product", productController.insertPhysicalProduct);
+router.post("/add-digital-product", productController.insertDigitalProduct);
 
 module.exports = router;
