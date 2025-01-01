a77c215a24c38797d011d4f9ad69909eac8fb524
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2025-01-01 23:10:19 +0530

    add getPhysicalProductWithDetails and getAllPhysicalProducts

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const productController = require("../controllers/product.controller");
 
 router.post('/add-physical-product', productController.insertPhysicalProduct);
+router.get('/get-physical-product/:id', productController.getPhysicalProductWithDetails);
+router.get('/get-all-physical-product', productController.getAllPhysicalProducts);
 
 module.exports = router;
