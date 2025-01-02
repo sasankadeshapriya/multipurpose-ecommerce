81ca9a7b9231e26824ac5545c01151d8fd132644
MaleeshaHe <malishaherath77@gmail.com>
2025-01-02 20:27:49 +0530

    create updateDigitalProduct function

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
 
-router.post('/add-physical-product', productController.insertPhysicalProduct);
-router.get('/get-physical-product/:id', productController.getPhysicalProductWithDetails);
-router.get('/get-all-physical-product', productController.getAllPhysicalProducts);
-router.delete('/delete-physical-product/:id', productController.deletePhysicalProduct);
+router.post("/add-physical-product", productController.insertPhysicalProduct);
+router.get(
+  "/get-physical-product/:id",
+  productController.getPhysicalProductWithDetails
+);
+router.get(
+  "/get-all-physical-product",
+  productController.getAllPhysicalProducts
+);
+router.delete(
+  "/delete-physical-product/:id",
+  productController.deletePhysicalProduct
+);
 
 router.post("/add-digital-product", productController.insertDigitalProduct);
-
+router.patch(
+  "/update-digital-product/:id",
+  productController.updateDigitalProduct
+);
 
 module.exports = router;
