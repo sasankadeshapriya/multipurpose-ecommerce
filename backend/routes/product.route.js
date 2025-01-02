75ab0fec535ffb1bff2a5128d42273fc971b0e50
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2025-01-02 19:35:12 +0530

    add deletePhysicalProduct

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,6 +5,7 @@ const productController = require("../controllers/product.controller");
 router.post('/add-physical-product', productController.insertPhysicalProduct);
 router.get('/get-physical-product/:id', productController.getPhysicalProductWithDetails);
 router.get('/get-all-physical-product', productController.getAllPhysicalProducts);
+router.delete('/delete-physical-product/:id', productController.deletePhysicalProduct);
 
 router.post("/add-digital-product", productController.insertDigitalProduct);
 
