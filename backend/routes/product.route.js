533f6c3c0f19f116b3241665cb99fc53b0cc9bd4
Maleesha Herath <malishaherath77@gmail.com>
2025-01-06 16:25:23 +0530

    Create digital products data retrivel functions

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,23 +3,14 @@ const router = express.Router();
 const productController = require("../controllers/product.controller");
 
 router.post("/add-physical-product", productController.insertPhysicalProduct);
-router.get(
-  "/get-physical-product/:id",
-  productController.getPhysicalProductWithDetails
-);
-router.get(
-  "/get-all-physical-product",
-  productController.getAllPhysicalProducts
-);
-router.delete(
-  "/delete-physical-product/:id",
-  productController.deletePhysicalProduct
-);
+router.get("/get-physical-product/:id", productController.getPhysicalProductWithDetails);
+router.get("/get-all-physical-product", productController.getAllPhysicalProducts);
+router.delete("/delete-physical-product/:id", productController.deletePhysicalProduct);
 
 router.post("/add-digital-product", productController.insertDigitalProduct);
-router.patch(
-  "/update-digital-product/:id",
-  productController.updateDigitalProduct
-);
+router.patch("/update-digital-product/:id", productController.updateDigitalProduct);
+router.delete("/delete-digital-product/:id", productController.deleteDigitalProduct);
+router.get("/get-digital-product/:id", productController.getDigitalProductWithDetails);
+router.get("/get-all-digital-product", productController.getAllDigitalProducts);
 
 module.exports = router;
